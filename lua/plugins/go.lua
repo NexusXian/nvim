f3296bf971454dcafbdb2e63dd919564881be962
Nexus Xian <xyhcatrl@gmail.com>
2025-06-08 20:23:53 +0800

    修复了go的高亮问题

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,26 +1,18 @@
 return {
   "ray-x/go.nvim",
-  dependencies = { -- optional packages
-    "ray-x/guihua.lua",
+  dependencies = {
+    "ray-x/guihua.lua", -- 浮窗依赖，必须
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {
-    -- lsp_keymaps = false,
-    -- other options
-  },
-  config = function(lp, opts)
-    require("go").setup(opts)
-    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-        require('go.format').goimports()
-      end,
-      group = format_sync_grp,
+  ft = { "go", "gomod" },
+  config = function()
+    require("go").setup({
+      lsp_cfg = true,
+      lsp_on_attach = true,
+      dap_debug = false, -- 明确禁用调试
+      trouble = false,
     })
   end,
-  event = { "CmdlineEnter" },
-  ft = { "go", 'gomod' },
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  build = ':lua require("go.install").update_all_sync()',
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,27 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   opts = {
     ensure_installed = {
-      'c', 'cpp', 'go', 'lua', 'html', 'css', 'javascript', 'java', 'dart', 'python', 'rust', 'typescript', 'tsx',
-      'graphql', 'vue', 'yaml', 'json', 'markdown', 'toml', 'html', 'css', 'javascript', 'java', 'dart', 'python', 'rust',
-      'typescript', 'tsx', 'graphql', 'vue', 'yaml', 'json', 'markdown', 'toml', 'html', 'css', 'javascript', 'java',
-      'dart', 'python', 'rust', 'typescript', 'tsx', 'graphql', 'vue', 'yaml', 'json', 'markdown', 'toml',
+      "c", "cpp", "go", "gomod", "lua", "html", "css", "javascript", "java", "dart", "python",
+      "rust", "typescript", "tsx", "graphql", "vue", "yaml", "json", "markdown", "toml"
     },
     auto_install = true,
-    highlight = { enable = true },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
     indent = { enable = true },
   },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+
+    -- 为 Go 文件添加自动命令确保高亮启用
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "go",
+      callback = function()
+        vim.cmd("TSBufEnable highlight")
+      end,
+    })
+  end,
 }
