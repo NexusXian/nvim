0fa68f98a5517d296740e6ee5ddd8c0022474ee2
Nexus Xian <xyhcatrl@gmail.com>
2025-07-28 21:51:12 +0800

    进一步美化了缩进显示

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,33 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = {
+      char = "│", -- 缩进线字符
+      tab_char = "│", -- Tab字符的缩进线
+    },
+    scope = {
+      enabled = true,                            -- 启用范围高亮
+      show_start = true,                         -- 显示范围起始位置
+      show_end = true,                           -- 显示范围结束位置
+      highlight = { "Function", "Label" },       -- 高亮组
+    },
+    whitespace = {
+      remove_blankline_trail = true,       -- 移除空行末尾的缩进线
+    },
+    exclude = {
+      filetypes = {       -- 排除的文件类型
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
 }
