e5d114227501275d5523a0bf69062b47997aab99
Nexus Xian <xyhcatrl@gmail.com>
2025-08-27 11:25:13 +0800

    添加了clnag的一系列支持，主要支持了c++23的新特性的lsp

--- lua/config/lspconfig/clangd.lua
+++ lua/config/lspconfig/clangd.lua
@@ -3,4 +3,16 @@ local lsp_common = require('config.lspconfig.init')
 
 lspconfig.clangd.setup({
   capabilities = lsp_common.capabilities,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--all-scopes-completion",
+    "--clang-tidy",
+    "--completion-style=detailed",
+    "--header-insertion=never",
+    -- 可以根据需要添加其他clangd参数
+  },
+  init_options = {
+    fallbackFlags = { "-std=c++23" }  -- 这里是关键修正
+  }
 })
