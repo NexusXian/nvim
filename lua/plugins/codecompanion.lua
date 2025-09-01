41334ee947e3b578123b3e3eb9e5fa40f2eb03e9
NexusXian <xyhcatrl@gmail.com>
2025-09-01 16:41:25 +0800

    采用了codecompanion的新写法，启用了旧写法

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -8,118 +8,120 @@ return {
   config = function()
     require("codecompanion").setup({
       adapters = {
-        opts = {
-          -- show_defaults 会导致copilot不能正常工作
-          show_defaults = true,
-          -- log_level = "DEBUG",
-        },
+        http = {
+          opts = {
+            -- show_defaults 会导致copilot不能正常工作
+            show_defaults = true,
+            -- log_level = "DEBUG",
+          },
 
-        deepseek = function()
-          return require("codecompanion.adapters").extend("deepseek", {
-            name = "deepseek",
-            env = {
-              api_key = function()
-                return os.getenv("DEEPSEEK_API_KEY")
-              end,
-            },
-            schema = {
-              model = {
-                default = "deepseek-coder",
+          deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              name = "deepseek",
+              env = {
+                api_key = function()
+                  return os.getenv("DEEPSEEK_API_KEY")
+                end,
               },
-            },
-          })
-        end,
-
-        siliconflow_r1 = function()
-          return require("codecompanion.adapters").extend("deepseek", {
-            name = "siliconflow_r1",
-            url = "https://api.siliconflow.cn/v1/chat/completions",
-            env = {
-              api_key = function()
-                return os.getenv("DEEPSEEK_API_KEY_S")
-              end,
-            },
-            schema = {
-              model = {
-                default = "deepseek-ai/DeepSeek-R1",
-                choices = {
-                  ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
-                  "deepseek-ai/DeepSeek-V3",
+              schema = {
+                model = {
+                  default = "deepseek-coder",
                 },
               },
-            },
-          })
-        end,
+            })
+          end,
 
-        siliconflow_v3 = function()
-          return require("codecompanion.adapters").extend("deepseek", {
-            name = "siliconflow_v3",
-            url = "https://api.siliconflow.cn/v1/chat/completions",
-            env = {
-              api_key = function()
-                return os.getenv("DEEPSEEK_API_KEY_S")
-              end,
-            },
-            schema = {
-              model = {
-                default = "deepseek-ai/DeepSeek-V3",
-                choices = {
-                  "deepseek-ai/DeepSeek-V3",
-                  ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+          siliconflow_r1 = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              name = "siliconflow_r1",
+              url = "https://api.siliconflow.cn/v1/chat/completions",
+              env = {
+                api_key = function()
+                  return os.getenv("DEEPSEEK_API_KEY_S")
+                end,
+              },
+              schema = {
+                model = {
+                  default = "deepseek-ai/DeepSeek-R1",
+                  choices = {
+                    ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+                    "deepseek-ai/DeepSeek-V3",
+                  },
                 },
               },
-            },
-          })
-        end,
+            })
+          end,
 
-        aliyun_deepseek = function()
-          return require("codecompanion.adapters").extend("deepseek", {
-            name = "aliyun_deepseek",
-            url = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
-            env = {
-              api_key = function()
-                return os.getenv("DEEPSEEK_API_ALIYUN")
-              end,
-            },
-            schema = {
-              model = {
-                default = "deepseek-r1",
-                choices = {
-                  ["deepseek-r1"] = { opts = { can_reason = true } },
+          siliconflow_v3 = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              name = "siliconflow_v3",
+              url = "https://api.siliconflow.cn/v1/chat/completions",
+              env = {
+                api_key = function()
+                  return os.getenv("DEEPSEEK_API_KEY_S")
+                end,
+              },
+              schema = {
+                model = {
+                  default = "deepseek-ai/DeepSeek-V3",
+                  choices = {
+                    "deepseek-ai/DeepSeek-V3",
+                    ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+                  },
                 },
               },
-            },
-          })
-        end,
+            })
+          end,
 
-        aliyun_qwen = function()
-          return require("codecompanion.adapters").extend("openai_compatible", {
-            name = "aliyun_qwen",
-            env = {
-              url = "https://dashscope.aliyuncs.com",
-              api_key = function()
-                return os.getenv("DEEPSEEK_API_ALIYUN")
-              end,
-              chat_url = "/compatible-mode/v1/chat/completions",
-            },
-            schema = {
-              model = {
-                default = "qwen3-coder-plus",
+          aliyun_deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              name = "aliyun_deepseek",
+              url = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+              env = {
+                api_key = function()
+                  return os.getenv("DEEPSEEK_API_ALIYUN")
+                end,
               },
-            },
-          })
-        end,
+              schema = {
+                model = {
+                  default = "deepseek-r1",
+                  choices = {
+                    ["deepseek-r1"] = { opts = { can_reason = true } },
+                  },
+                },
+              },
+            })
+          end,
 
-        copilot_claude = function()
-          return require("codecompanion.adapters").extend("copilot", {
-            name = "copilot_claude",
-            schema = {
-              model = {
-                default = "claude-3.7-sonnet",
+          aliyun_qwen = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              name = "aliyun_qwen",
+              env = {
+                url = "https://dashscope.aliyuncs.com",
+                api_key = function()
+                  return os.getenv("DEEPSEEK_API_ALIYUN")
+                end,
+                chat_url = "/compatible-mode/v1/chat/completions",
               },
-            },
-          })
-        end,
+              schema = {
+                model = {
+                  default = "qwen3-coder-plus",
+                },
+              },
+            })
+          end,
+
+          copilot_claude = function()
+            return require("codecompanion.adapters").extend("copilot", {
+              name = "copilot_claude",
+              schema = {
+                model = {
+                  default = "claude-3.7-sonnet",
+                },
+              },
+            })
+          end,
+        }
       },
 
       strategies = {
