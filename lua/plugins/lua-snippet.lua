a97ad3174f33189f608c0cf9d49c98bf2da01450
NexusXian <xyhcatrl@gmail.com>
2026-01-29 00:01:12 +0800

    added markdown features

---
+++ lua/plugins/lua-snippet.lua
@@ -0,0 +1,10 @@
+return {
+  "L3MON4D3/LuaSnip",
+  version = "v2.*",
+  config = function()
+    require("luasnip.loaders.from_lua").lazy_load({
+      paths = vim.fn.stdpath("config") .. "/lua/snippets",
+    })
+  end,
+}
+
