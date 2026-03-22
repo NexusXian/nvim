fb8f59a317548baf505cdd888c9bcaebe7b9d5c2
NexusXian <xyhcatrl@gmail.com>
2026-03-22 13:45:29 +0800

    添加了autotag的配置

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,6 +1,9 @@
 return {
   'windwp/nvim-ts-autotag',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  opts = {},
+  opts = {
+    enable_close = true,         -- 自动补全结束标签
+    enable_rename = true,        -- 改开始标签时同步改结束标签
+    enable_close_on_slash = true,
+  },
 }
-
