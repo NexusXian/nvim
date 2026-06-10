a67f3f32957e3da4daa3a9fa4fa00450dd4c42b0
NexusXian <xyhcatrl@gmail.com>
2026-06-10 21:40:46 +0800

    feat:support the nvim 0.12.* liveserver plugins syntax

--- lua/plugins/liveserver.lua
+++ lua/plugins/liveserver.lua
@@ -2,8 +2,20 @@ return {
   {
     "barrett-ruth/live-server.nvim",
     build = "npm install -g live-server",
-    cmd = { "LiveServerStart", "LiveServerStop" },
-    ft = "html", 
-    config = true,
+    cmd = { "LiveServerStart", "LiveServerStop", "LiveServerToggle" },
+    ft = "html",
+    init = function()
+      vim.g.live_server = {
+        port = 8200,
+        browser = "default",
+      }
+    end,
+    keys = {
+      {
+        "<leader>ls",
+        "<cmd>LiveServerToggle<CR>",
+        desc = "Toggle Live Server",
+      },
+    },
   },
 }
